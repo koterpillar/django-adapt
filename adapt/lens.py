8afc9efd826e9837833b0331a8ad0d6a168edd9f
Alexey Kotlyarov <a@koterpillar.com>
2017-03-12 17:02:11 +1100

    Composing lens

--- adapt/lens.py
+++ adapt/lens.py
@@ -19,6 +19,27 @@ class Lens(metaclass=ABCMeta):
         """Update the value in the object."""
         pass
 
+    def __mul__(self, inner: 'Lens') -> 'Lens':
+        """Compose lenses."""
+
+        return Composed(self, inner)
+
+
+class Composed(Lens):
+    """A composition of two lenses."""
+
+    def __init__(self, outer: Lens, inner: Lens) -> None:
+        self.outer = outer
+        self.inner = inner
+
+    def get(self, target: T) -> Any:
+        return self.inner.get(self.outer.get(target))
+
+    def set(self, target: T, value: Any) -> T:
+        inner_target = self.outer.get(target)
+        inner_target_ = self.inner.set(inner_target, value)
+        return self.outer.set(target, inner_target_)
+
 
 class Attribute(Lens):
     """Lens targeting an object's specific attribute."""
--- tests/test_lens.py
+++ tests/test_lens.py
@@ -1,21 +1,35 @@
 """Test lenses."""
 
 import unittest
+from typing import Optional
 
 from adapt.lens import Attribute
 
 
-class Person:
-    """Class to test attribute lens."""
+class Address:
+    """
+    Sample address class.
 
-    def __init__(self, name: str, email: str) -> None:
+    Real addresses aren't as simple as a street and a number, and a "street
+    number" isn't really a number anyway.
+    """
+
+    def __init__(self, street: str, number: int) -> None:
+        self.street = street
+        self.number = number
+
+class Person:
+    """Sample person class."""
+
+    def __init__(self, name: str, email: str, address: Optional[Address] = None) -> None:
         self.name = name
         self.email = email
+        self.address = address
 
 
 class TestAttribute(unittest.TestCase):
 
-    def test_lens(self):
+    def test_lens(self) -> None:
         name = Attribute('name')
 
         person = Person("Ayano", "ayano@naver.com")
@@ -23,3 +37,20 @@ class TestAttribute(unittest.TestCase):
 
         person_ = name.set(person, "Nocchi")
         self.assertEqual(person_.name, "Nocchi")
+
+
+class TestCompose(unittest.TestCase):
+
+    def test_lens(self) -> None:
+        address_lens = Attribute('address')
+        number = Attribute('number')
+        address_number = address_lens * number
+
+        address = Address("Banpo", 12)
+        person = Person("Ayano", "ayano@naver.com", address)
+
+        self.assertEqual(address_number.get(person), 12)
+
+        person_ = address_number.set(person, 14)
+        assert person_.address is not None
+        self.assertEqual(person_.address.number, 14)
