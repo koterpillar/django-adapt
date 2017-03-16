4f6d1d753cda91ef883af619e194e92460d65057
Alexey Kotlyarov <a@koterpillar.com>
2017-03-16 17:57:03 +1100

    Test nested updates

--- tests/test_django.py
+++ tests/test_django.py
@@ -16,8 +16,9 @@ class TestDjango(unittest.TestCase):
         os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.sample_app.settings'
         django.setup()
 
-        call_command('check')
-        call_command('migrate', run_syncdb=True)
+        with open('/dev/null', 'w') as null:
+            call_command('check', verbosity=0, stdout=null)
+        call_command('migrate', run_syncdb=True, verbosity=0)
 
     def test_model(self) -> None:
         """Test updating a model."""
@@ -49,3 +50,51 @@ class TestDjango(unittest.TestCase):
         address_obj_.refresh_from_db()
         self.assertEqual(address_obj_.street, "Gangnam")
         self.assertEqual(address_obj_.number, 25)
+
+    def test_nested(self) -> None:
+        """Test updating a nested model."""
+
+        from .sample_app.models import Address, User
+
+        address_obj = Address.objects.create(
+            street="Banpo",
+            number=12,
+        )
+        user_obj = User.objects.create(
+            name="Ayano",
+            email="ayano@example.com",
+            address=address_obj,
+        )
+
+        user = Model({
+            'name': string,
+            'email': string,
+            'address': Model({
+                'street': string,
+                'number': integer,
+            }),
+        })
+
+        self.assertEqual(user.get(user_obj), {
+            'name': "Ayano",
+            'email': "ayano@example.com",
+            'address': {
+                'street': "Banpo",
+                'number': 12,
+            },
+        })
+
+        user_obj_ = user.set(user_obj, {
+            'name': "Nocchi",
+            'email': "nocchi@example.com",
+            'address': {
+                'street': "Gangnam",
+                'number': 25,
+            },
+        })
+
+        self.assertEqual(user_obj_.name, "Nocchi")
+
+        # Old address must have been updated in-place
+        address_obj.refresh_from_db()
+        self.assertEqual(address_obj.street, "Gangnam")
